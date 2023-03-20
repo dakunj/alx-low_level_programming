91d73caa227902cdf1a4afe6c4584b9a6f8c6d67
dakunj <dakunj@gmail.com>
2023-03-20 20:08:21 +0000

    Print all alphabet in lowercase each on new lines while ommiting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - function to print alphabet in lowercase missing q and e followed by a new line
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int c;
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		if (x == 'E' || x == 'Q')
+		{
+			continue;
+		}
+		c = tolower(x);
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
+
