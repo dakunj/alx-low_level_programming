8b141c98add23d6093d411a195e330a898ff7114
dakunj <dakunj@gmail.com>
2023-03-20 23:39:46 +0000

    Print all the possible combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,13 @@ int main(void)
 	for (x = 0; x <= 9; x++)
 	{
 		putchar (x + '0');
-	if(x == 9)
+	if (x == 9)
 	{
 		break;
 	}
 	putchar (',');
 	putchar (' ');
 	}
+	putchar ('\n');
 	return (0);
 }
