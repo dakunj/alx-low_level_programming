ed0f64496f93245d5b01ae2e8366741f77768170
dakunj <dakunj@gmail.com>
2023-03-24 21:36:51 +0000

    Prints a function that checks for lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,15 @@
 
 /**
  * _islower - function that checks the lowercase character
- *
- * Return: ALways 0.
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }
