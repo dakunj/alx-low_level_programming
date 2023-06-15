57ba5dab7b618c928a91104bddf94919aa02b5dc
dakunj <dakunj@gmail.com>
2023-06-15 17:43:04 +0000

    Puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _puts_recursion - Write a function that prints a string, followed by a new line.
+ * _puts_recursion - Write a function that prints a string, 
+ * followed by a new line.
  *
  * @s: This is the input string
  */
