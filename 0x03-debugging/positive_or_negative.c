1f826e6e513b9a9f2c7eee9c6d11efd4b061796b
dakunj <dakunj@gmail.com>
2023-07-04 08:25:18 +0000

    Deferment

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include "main.h"
 
 /**
  *  main - to determine if a number is positive, negative or zero
@@ -14,10 +12,12 @@ void positive_or_negative(int i)
 	if (i > 0)
 	{
 		printf("%d is positive\n", i);
-	} else if (i < 0)
+	}
+	else if (i < 0)
 	{
 		printf("%d is negative\n", i);
-	} else
+	}
+	else
 	{
 		printf("%d is zero\n", i);
 	}
